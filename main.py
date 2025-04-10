aad4752745081d13aa559daac350083430e7d84a
gkn2 <112503427+gkn2@users.noreply.github.com>
2025-04-10 11:34:37 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,19 @@
+greeting = "Hello!"
+
+
+
+
+def change_greeting():
+    global greeting
+    greeting = "Welcome to the game!"
+
+
+
+
+mySprite = sprites.create(assets.image("mc"), SpriteKind.player)
+
+
+
+
+change_greeting()
+mySprite.say(greeting)
